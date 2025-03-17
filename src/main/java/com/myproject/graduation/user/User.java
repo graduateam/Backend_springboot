995f92ec6e78717021a5624c8ac30ff640363c28
woohyun23 <parkwh0823@gmail.com>
2025-03-17 15:09:41 +0900

    feat) User.java 형식 수정

--- src/main/java/com/myproject/graduation/user/User.java
+++ src/main/java/com/myproject/graduation/user/User.java
@@ -1,9 +1,15 @@
 package com.myproject.graduation.user;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -13,32 +19,16 @@ public class User {
     @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     @Column(name = "password", nullable = false)
     private String password;
 
     @Column(name = "user_status", nullable = false)
+    @Enumerated(EnumType.STRING)
     private UserStatus status;
 
     @Version
     private Long version;
-
-    // 기본 생성자
-    public User() { }
-
-    // Getter & Setter
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getUsername() { return name; }
-    public void setUsername(String username) { this.name = username; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-    public UserStatus getStatus() { return status; }
-    public void setStatus(UserStatus status) { this.status = status; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
 }
