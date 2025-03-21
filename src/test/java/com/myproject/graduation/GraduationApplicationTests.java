fe26965059f1d13aa6ce78ec675bccd072d4a5ca
woohyun23 <parkwh0823@gmail.com>
2025-03-21 12:09:50 +0900

    env) ci시에만 H2로 DB 수정

--- src/test/java/com/myproject/graduation/GraduationApplicationTests.java
+++ src/test/java/com/myproject/graduation/GraduationApplicationTests.java
@@ -3,8 +3,10 @@ package com.myproject.graduation;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GraduationApplicationTests {
 
 	@Test
